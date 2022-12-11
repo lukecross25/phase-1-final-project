e5b352ee3d30ec89f149d9c2a9279d178dabc0e2
Luke Cross <luke.cross25@gmail.com>
2022-12-11 14:25:28 -0700

    added a fetch for the json file

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Wait for the DOM to load
 document.addEventListener('DOMContentLoaded', function() {
   const container = document.getElementById('container');
+
+  fetch('http://localhost:3000/cardinalPlayers')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(player => {
+      // Create a counter variable for each player
+      let favoriteCount = 0;
+      let allStarCount = 0;
+
+      // Create a new card element for each player
+      const card = document.createElement('div');
+      card.classList.add('card');
+
