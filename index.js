83b80af99128b6ad8d8611ad94cfd3fd9e3f327c
Luke Cross <luke.cross25@gmail.com>
2022-12-11 14:23:40 -0700

    added dom loaded event listner

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const playerCardContainer = document.getElementById('player-card-container');
-
-searchButton.addEventListener('click', () => {
-  const searchValue = searchInput.value;
-  fetch(`http://localhost:3000/cardinalPlayers?Name=${searchValue}`)
-    .then(response => response.json())
-    .then(players => {
-      playerCardContainer.innerHTML = '';
-      players.forEach(player => {
-        const playerCard = `
-          <div class="player-card">
-            <img src="${player.imageUrl}" alt="${player.Name}">
-            <div class="name">${player.Name}</div>
-            <div class="position">${player.Position}</div>
-            <div class="stats">${player['2022 Slash']}</div>
-            <div class="awards">${player.Awards}</div>
-          </div>
-        `;
-        playerCardContainer.innerHTML += playerCard;
-      });
-    });
-});
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+  const container = document.getElementById('container');
